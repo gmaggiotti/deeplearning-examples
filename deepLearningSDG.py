51c151b8683eb646bc5e6bab32397f9e7c010d34
gabe <gmaggiotti@gmail.com>
2017-05-27 09:41:53 -0300

    adding SGD improvement

--- deepLearningSDG.py
+++ deepLearningSDG.py
@@ -51,84 +51,65 @@ print('Test set', test_dataset.shape, test_labels.shape)
 # Subset the training data for faster turnaround.
 train_subset = 10000
 
+batch_size = 128
+
 graph = tf.Graph()
 with graph.as_default():
 
-    # Input data.
-    # Load the training, validation and test data into constants that are
-    # attached to the graph.
-    tf_train_dataset = tf.constant(train_dataset[:train_subset, :], name='train_set')
-    tf_train_labels = tf.constant(train_labels[:train_subset])
+    # Input data. For the training data, we use a placeholder that will be fed
+    # at run time with a training minibatch.
+    tf_train_dataset = tf.placeholder(tf.float32,
+                                      shape=(batch_size, image_size * image_size))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
     tf_valid_dataset = tf.constant(valid_dataset)
-    #tf_test_dataset = tf.constant(test_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
 
     # Variables.
-    # These are the parameters that we are going to be training. The weight
-    # matrix will be initialized using random values following a (truncated)
-    # normal distribution. The biases get initialized to zero.
     weights = tf.Variable(
         tf.truncated_normal([image_size * image_size, num_labels]))
     biases = tf.Variable(tf.zeros([num_labels]))
 
-
     # Training computation.
-    # We multiply the inputs with the weight matrix, and add biases. We compute
-    # the softmax and cross-entropy (it's one operation in TensorFlow, because
-    # it's very common, and it can be optimized). We take the average of this
-    # cross-entropy across all training examples: that's our loss.
     logits = tf.matmul(tf_train_dataset, weights) + biases
     loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
 
-    #this is for write output in tensorboard
-    # for value in [ tf_train_dataset, weights,biases,logits,loss ]:
-    #     tf.summary.scalar(value.op.name, value)
-    #
-    # summaries = tf.summary.merge_all()
-
-
     # Optimizer.
-    # We are going to find the minimum of this loss using gradient descent.
     optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
     # Predictions for the training, validation, and test data.
-    # These are not part of training, but merely here so that we can report
-    # accuracy figures as we train.
     train_prediction = tf.nn.softmax(logits)
     valid_prediction = tf.nn.softmax(
         tf.matmul(tf_valid_dataset, weights) + biases)
-    #test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
-
+    test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
 
 ##
-
-num_steps = 801
-
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
             / predictions.shape[0])
+####
+
+num_steps = 3001
 
 with tf.Session(graph=graph) as session:
-    # This is a one-time operation which ensures the parameters get initialized as
-    # we described in the graph: random weights for the matrix, zeros for the
-    # biases.
     tf.global_variables_initializer().run()
-    print('Initialized')
+    print("Initialized")
     for step in range(num_steps):
-        # Run the computations. We tell .run() that we want to run the optimizer,
-        # and get the loss value and the training predictions returned as numpy
-        # arrays.
-        _, l, predictions = session.run([optimizer, loss, train_prediction])
-        if (step % 100 == 0):
-            print('Loss at step %d: %f' % (step, l))
-            print('Training accuracy: %.1f%%' % accuracy(
-                predictions, train_labels[:train_subset, :]))
-            # Calling .eval() on valid_prediction is basically like calling run(), but
-            # just to get that one numpy array. Note that it recomputes all its graph
-            # dependencies.
-            print('Validation accuracy: %.1f%%' % accuracy(
+        # Pick an offset within the training data, which has been randomized.
+        # Note: we could use better randomization across epochs.
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        # Generate a minibatch.
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        # Prepare a dictionary telling the session where to feed the minibatch.
+        # The key of the dictionary is the placeholder node of the graph to be fed,
+        # and the value is the numpy array to feed to it.
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run(
+            [optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 500 == 0):
+            print("Minibatch loss at step %d: %f" % (step, l))
+            print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+            print("Validation accuracy: %.1f%%" % accuracy(
                 valid_prediction.eval(), valid_labels))
-
-    tf_test_dataset = tf.constant(test_dataset)
-    test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
-    print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
