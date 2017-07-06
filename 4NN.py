8bb947499457d1dee5a21434bf2f70c511e55d17
gabe <gmaggiotti@gmail.com>
2017-07-06 16:08:16 -0300

    adding 4 layer net

--- 4NN.py
+++ 4NN.py
@@ -31,7 +31,8 @@ np.random.seed(1)
 
 #synapses
 syn0 = 2*np.random.random((3,4)) - 1  # 3x4 matrix of weights ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
-syn1 = 2*np.random.random((4,1)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
+syn1 = 2*np.random.random((4,4)) - 1
+syn2 = 2*np.random.random((4,1)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
 
 
 # This is the main training loop. The output shows the evolution of the error between the model and desired. The error steadily decreases.
@@ -46,30 +47,36 @@ for j in range(60000):
     l0 = X
     l1 = sigmoid(np.dot(l0, syn0))
     l2 = sigmoid(np.dot(l1, syn1))
+    l3 = sigmoid(np.dot(l2, syn2))
 
     # Error back propagation of errors using the chain rule.
-    l2_error = y - l2
+    l3_error = y - l3
     if(j % 10000) == 0:   # Only print the error every 10000 steps, to save time and limit the amount of output.
-        print("Error: " + str(np.mean(np.abs(l2_error))))
+        print("Error: " + str(np.mean(np.abs(l3_error))))
+
+    l3_delta = l3_error*sigmoid(l3, deriv=True)
+    l2_error = l3_delta.dot(syn2.T)
 
     l2_delta = l2_error*sigmoid(l2, deriv=True)
-
     l1_error = l2_delta.dot(syn1.T)
 
     l1_delta = l1_error * sigmoid(l1,deriv=True)
 
     #update weights (no learning rate term)
+    syn2 += l2.T.dot(l3_delta)
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
 
 print("Output after training")
-print(l2)
+print(l3)
 
 def predict(X1):
     l0 = X1
     l1 = sigmoid(np.dot(l0, syn0))
     l2 = sigmoid(np.dot(l1, syn1))
-    return l2[0] #since process X1[0] output would be l2[0]
+    l3 = sigmoid(np.dot(l2, syn2))
+
+    return l3[0] #since process X1[0] output would be l2[0]
 
 X1 = np.array([[0,0,1],
               [0,0,0],
