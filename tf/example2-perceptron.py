83789ad01fe5cc64def47a58dcfb219c76fd9b12
gabe <gmaggiotti@gmail.com>
2018-02-21 10:56:47 -0300

    minor refactor

--- tf/example2-perceptron.py
+++ tf/example2-perceptron.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
+n_samples = 3
+n_neurons = 3
+x = tf.placeholder(tf.float32, shape=[n_samples, n_neurons])
+y = tf.placeholder(tf.float32, shape=[n_samples, 1])
 
-x = tf.placeholder(tf.float32, shape=[3, 2])
-y = tf.placeholder(tf.float32, shape=[3, 1])
+W = tf.Variable( tf.random_normal([n_neurons,1]), name="W", dtype=tf.float32)
+b = tf.Variable( tf.zeros([n_neurons,1]), name="bias", dtype=tf.float32)
 
-W = tf.Variable( tf.random_normal([2,1]), name="W", dtype=tf.float32)
-b = tf.Variable( tf.zeros([3,1]), name="bias", dtype=tf.float32)
+logits = tf.sigmoid(tf.matmul(x, W) + b)
+out = tf.reduce_mean( logits )
 
-logits = tf.matmul(x,W) + b
-out = tf.reduce_mean( tf.sigmoid(logits) )
-
+### calculate the error
 loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits( logits=logits, labels=y))
 LR = 0.02
+### run the optimization
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=LR).minimize(loss)
 
 
@@ -18,10 +21,14 @@ init = tf.global_variables_initializer()
 sess = tf.InteractiveSession() #initializes a tensorflow session
 sess.run(init)
 
-x_data = [[1.0,2.0],[1.0,2.0],[3.0,2.0]]
-y_data = [[1.0], [2.0], [5.0]]
-for epoch in range(500001):
+x_data = [[1.0, 2.0, 3.0], [0.0, 0, 0], [3.0, 2.0, 1.0]]
+y_data = [[1.0], [0], [1.0]]
+for epoch in range(1000001):
     if epoch % 50000 == 0:
-        sess.run(optimizer, feed_dict={x:x_data, y:y_data })
-        l, o = sess.run([loss, out], feed_dict={x:x_data, y:y_data })
+        ### run the optimizer
+        sess.run(optimizer, feed_dict={x: x_data, y: y_data})
+        l, o = sess.run([loss, out], feed_dict={x: x_data, y: y_data})
         print 'loss: ', l, ' error:', 1 - o
+
+
+print('EOC')
