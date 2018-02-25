66684b8fd32e94b5a5660e3960b059808e6d64aa
gabe <gmaggiotti@gmail.com>
2018-02-25 12:06:41 -0300

    fixing optimizer execution within the loop :S

--- tf/example2-perceptron.py
+++ tf/example2-perceptron.py
@@ -9,6 +9,8 @@ x_data = [
 ]
 y_data = [[1], [1], [0], [0]]
 
+x_data = 2*np.array(x_data)/30.0 -1
+
 n_samples = 4
 n_neurons = 7
 x = tf.placeholder(tf.float32, shape=[n_samples, n_neurons])
@@ -17,24 +19,27 @@ y = tf.placeholder(tf.float32, shape=[n_samples, 1])
 W = tf.Variable( tf.random_normal([n_neurons,1], seed=0), name="W", dtype=tf.float32)
 b = tf.Variable( tf.zeros([n_samples,1]), name="bias", dtype=tf.float32)
 
+logits = tf.sigmoid(tf.matmul(x, W) + b)
+out = tf.reduce_mean( logits )
 
-y_prime = tf.matmul(x, W) + b
-loss = tf.reduce_min( tf.square(y - y_prime))
+### calculate the error
+loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits( logits=logits, labels=y))
 
 LR = 0.01
 ### run the optimization
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=LR).minimize(loss)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate=LR).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=LR).minimize(loss)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())     #init W & b
     writer = tf.summary.FileWriter("./tensorboard_log", sess.graph)
 
-    for epoch in range(360000):
-        if epoch % 40000 == 0:
-            ### run the optimizer
-            opt, lo = sess.run([optimizer, loss], feed_dict={x: x_data, y: y_data})
+    for epoch in range(1000):
+        ### run the optimizer
+        opt, lo = sess.run([optimizer, loss], feed_dict={x: x_data, y: y_data})
+        if epoch % 100 == 0:
             print lo
 
-
+    print "y_prime: ",sess.run(logits, feed_dict={x: x_data,y: y_data })
     print('EOC')
 
---
+++ tf/example2b-linear.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+
+x_data = [1, 2, 3, 4]
+y_data = [0, -1, -2, -3]
+
+
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+
+W = tf.Variable(tf.random_normal([1,1], seed=0), name="W", dtype=tf.float32)
+b = tf.Variable(tf.zeros([1,1]), name="bias", dtype=tf.float32)
+
+
+y_prime = W * x + b
+sqr_delta = tf.square(y_prime - y)
+loss = tf.reduce_sum( sqr_delta )
+
+LR = 0.01
+### run the optimization
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=LR).minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())     #init W & b
+    writer = tf.summary.FileWriter("./tensorboard_log", sess.graph)
+
+    for epoch in range(1000):
+        ### run the optimizer
+        opt, lo = sess.run([optimizer, loss], feed_dict={x: x_data, y: y_data})
+        if epoch % 100 == 0:
+            print lo
+
+    print sess.run(loss, feed_dict={x: x_data,y: y_data })
+    print sess.run([W,b])
+    print('EOC')
+
