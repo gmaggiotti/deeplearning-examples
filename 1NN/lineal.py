a6a2878feebb7e260798ee7fcec06d72a6994630
gabe <gmaggiotti@gmail.com>
2020-02-06 09:14:55 -0300

    minor changes

---
+++ 1NN/lineal.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+# # input data, each column represent a dif neuron
+# X = np.loadtxt("1NN/X.txt", delimiter=",")
+#
+# # output, are the one-hot encoded labels
+# y = np.loadtxt("1NN/Y.txt", delimiter=",").reshape(X.__len__(), 1)
+
+X = np.arange(10)
+delta = np.random.uniform(-2,2, size=(10,))
+y = .6 * X + delta
+
+np.random.seed(1)  # The seed for the random generator is set so that it will return the same random numbers each time,
+# which is sometimes useful for debugging.
+
+# Now we intialize the weights to random values. w0 is the weight between the input layer and the hidden layer.
+
+# synapses
+w0 = np.random.random()
+
+# This is the main training loop. The output shows the evolution of the error between the model and desired. The
+# error steadily decreases.
+pre_error= 1
+error = 0
+for j in range(100):
+
+    # Calculate forward through the network.
+    l1 = X*w0
+
+    # Error back propagation of errors using the chain rule.
+    l1_error = y.T - l1
+    error = np.mean(np.abs(l1_error))
+    print("epoch {}: {}".format(j, error))
+    if(error > pre_error):
+        print("fue")
+
+    adjustment = np.abs(l1_error * (X))
+    error = pre_error
+
+    # update weights (no learning rate term)
+    w0 += adjustment.sum()/adjustment.size * 0.001
+
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
+
+
+plt.plot(X, y, 'o', color='black');
+
+x1 = np.linspace(0, 10, 50)
+y1 = x1*w0
+plt.plot(x1, y1, 'o', color='green');
+plt.show()
+
