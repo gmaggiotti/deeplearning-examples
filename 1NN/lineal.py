7dbc6e15cf2f628d896f3cca36b27bc5af1b2bdd
gabe <gmaggiotti@gmail.com>
2020-02-06 09:34:29 -0300

    minor fix

--- 1NN/lineal.py
+++ 1NN/lineal.py
@@ -7,8 +7,8 @@ import numpy as np
 # # output, are the one-hot encoded labels
 # y = np.loadtxt("1NN/Y.txt", delimiter=",").reshape(X.__len__(), 1)
 
-X = np.arange(10)
-delta = np.random.uniform(-2,2, size=(10,))
+X = np.arange(50)
+delta = np.random.uniform(-2,2, size=(50,))
 y = .6 * X + delta
 
 np.random.seed(1)  # The seed for the random generator is set so that it will return the same random numbers each time,
@@ -21,9 +21,9 @@ w0 = np.random.random()
 
 # This is the main training loop. The output shows the evolution of the error between the model and desired. The
 # error steadily decreases.
-pre_error= 1
+pre_error= 100
 error = 0
-for j in range(100):
+for j in range(400):
 
     # Calculate forward through the network.
     l1 = X*w0
@@ -33,10 +33,10 @@ for j in range(100):
     error = np.mean(np.abs(l1_error))
     print("epoch {}: {}".format(j, error))
     if(error > pre_error):
-        print("fue")
+        break
 
     adjustment = np.abs(l1_error * (X))
-    error = pre_error
+    pre_error = error
 
     # update weights (no learning rate term)
     w0 += adjustment.sum()/adjustment.size * 0.001
@@ -49,7 +49,7 @@ import numpy as np
 
 plt.plot(X, y, 'o', color='black');
 
-x1 = np.linspace(0, 10, 50)
+x1 = np.linspace(0, 50, 50)
 y1 = x1*w0
 plt.plot(x1, y1, 'o', color='green');
 plt.show()
